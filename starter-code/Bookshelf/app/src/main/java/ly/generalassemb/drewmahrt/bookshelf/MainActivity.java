00ab4c0799ad1590dab464cfeb5e542cf0165a22
James <jamesrondina@gmail.com>
2016-07-04 23:38:19 -0400

    jrondina made a quick stab at toggle functionality, didn't work

--- starter-code/Bookshelf/app/src/main/java/ly/generalassemb/drewmahrt/bookshelf/MainActivity.java
+++ starter-code/Bookshelf/app/src/main/java/ly/generalassemb/drewmahrt/bookshelf/MainActivity.java
@@ -1,32 +1,37 @@
 package ly.generalassemb.drewmahrt.bookshelf;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
-
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    //int count = 1;
     BaseAdapter mBookAdapter;
 
     //TODO: Define your ListView
 
+    ListView listView;
+
     //TODO: Define your Book List
+
+    List<Book> mBookList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.list_view);
 
         //Use helper method to add books to the list
         mBookList = generateBooks();
@@ -36,26 +41,68 @@ public class MainActivity extends AppCompatActivity {
         mBookAdapter = new BaseAdapter() {
             @Override
             public int getCount() {
+
                 return mBookList.size();
             }
 
             @Override
             public Object getItem(int position) {
+
                 return mBookList.get(position);
             }
 
             @Override
             public long getItemId(int position) {
+
                 return position;
             }
 
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
                 //TODO: Update the view
+                View view = convertView;
+
+                if (view == null) {
+                    LayoutInflater inflate =  (LayoutInflater) MainActivity.
+                            this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    view = inflate.inflate(android.R.layout.simple_list_item_2, null);
+                }
+
+                TextView titleText = (TextView) view.findViewById(android.R.id.text1);
+                TextView authorText = (TextView) view.findViewById(android.R.id.text2);
+
+                titleText.setText("Title: " + mBookList.get(position).getTitle());
+                authorText.setText("Author: " + mBookList.get(position).getAuthor());
+
+                return view;
             }
         };
 
         //TODO: Set the ListView's adapter
+
+        listView.setAdapter(mBookAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                TextView titleText = (TextView) view.findViewById(android.R.id.text1);
+                TextView authorText = (TextView) view.findViewById(android.R.id.text2);
+
+                //if ((count % 2) == 1) {
+                    titleText.setTextColor(Color.parseColor("#FF0000"));
+                    authorText.setTextColor(Color.parseColor("#FF0000"));
+                /*}
+                else {
+                    titleText.setTextColor(Color.parseColor("#000000"));
+                    authorText.setTextColor(Color.parseColor("#000000"));
+                }
+
+                count++;*/
+
+            }
+        });
+
 
     }
 
